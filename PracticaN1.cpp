cbabdc9bbdba732a0c5b42574d0046d10a032365
Stalyncachaguay <stalynsebastian1999@gmail.com>
2025-06-23 11:09:43 -0500

    Add files via upload

---
+++ PracticaN1.cpp
@@ -0,0 +1,12 @@
+// Práctica N.1 Stalyn Cachaguay
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string saludo = "Hola mundo ";
+  cout << saludo << endl;
+  return 0;
+}
