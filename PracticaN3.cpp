fede6148d02cbd5b2fc5ac62a66c784bc29476a9
Stalyncachaguay <stalynsebastian1999@gmail.com>
2025-06-23 11:10:36 -0500

    Add files via upload

---
+++ PracticaN3.cpp
@@ -0,0 +1,15 @@
+// Práctica N.3 Stalyn Cachaguay
+// Ej. 1
+// Escribe un programa que imprima los números del 1 al 10.
+
+#include <iostream> 
+#include <string>  
+
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 10; i++) {
+        cout << i << endl; 
+    }
+    return 0; 
+}
